2f5d372b19ea078c4e13a73c7125a04428290653
ananooo <ananoo@icloud.com>
2020-01-07 12:29:48 +0800

    first commit

---
+++ 剑指offer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  剑指offer
+//
+//  Created by ananooo on 2020/1/7.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
